2ffb74d591ed793b6daf27bac4c5905e45d9b63b
cosmicorganism <cosmicorganism@gmail.com>
2013-10-10 08:22:15 -0700

    fixed a bug related to indexing into the the 1D array with a 3D index. Created a new method that encapsulates this functiionality

--- src/chunk_array.hpp
+++ src/chunk_array.hpp
@@ -16,25 +16,31 @@ private:
 
   block_type get_impl(const local_size_t x, const local_size_t y,
                       const local_size_t z) const override;
-  void set_impl(const local_size_t x, const local_size_t y, const local_size_t z,
-                const block_type type) override;
+  void set_impl(const local_size_t x, const local_size_t y,
+                const local_size_t z, const block_type type) override;
   bool is_solid_impl(const local_size_t x, const local_size_t y,
                      const local_size_t z) const override;
   bool is_transparent_impl(const local_size_t x, const local_size_t y,
-                     const local_size_t z) const override;};
+                           const local_size_t z) const override;
+  inline auto get_1D_index(
+      const local_size_t x, const local_size_t y,
+      const local_size_t z) const -> decltype(chunk_base_whd::volume) {
+    return x + W * y + W * H * z;
+  }
+};
 
 template <local_size_t W, local_size_t H, local_size_t D>
 block_type
 array_chunk<W, H, D>::get_impl(const local_size_t x, const local_size_t y,
                                const local_size_t z) const {
-  return data[x + H * y + H * D * z];
+  return data[get_1D_index(x, y, z)];
 }
 
 template <local_size_t W, local_size_t H, local_size_t D>
 void array_chunk<W, H, D>::set_impl(const local_size_t x, const local_size_t y,
                                     const local_size_t z,
                                     const block_type type) {
-  auto &current_type = data[x + H * y + H * D * z];
+  auto &current_type = data[get_1D_index(x, y, z)];
   if (current_type != type) {
     current_type = type;
     chunk_base_whd::mark_dirty();
@@ -45,14 +51,15 @@ template <local_size_t W, local_size_t H, local_size_t D>
 bool array_chunk<W, H, D>::is_solid_impl(const local_size_t x,
                                          const local_size_t y,
                                          const local_size_t z) const {
-  return data[x + H * y + H * D * z] != block_type::air;
+  return data[get_1D_index(x, y, z)] != block_type::air;
 }
 
 template <local_size_t W, local_size_t H, local_size_t D>
-bool array_chunk<W, H, D>::is_transparent_impl(const local_size_t ,
-                                         const local_size_t ,
-                                         const local_size_t ) const {
-  //return data[x + H * y + H * D * z] == block_type::water;
+bool array_chunk<W, H, D>::is_transparent_impl(const local_size_t,
+                                               const local_size_t,
+                                               const local_size_t) const {
+  // return data[get_1D_index(x, y, z)] == block_type::water;
   return false;
 }
+
 } // namespace lexov
